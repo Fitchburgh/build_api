1ef51d6272869c51dfe5638c18d7c109ca1309d6
Brandon Fitch <fitchbj@gmail.com>
2016-10-04 22:12:56 -0400

    Readme updated.  files updated. attempted sorting. attempted html interfacing.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # build_api
+
+installation - clone/download build_api folder and contents and save to desired
+directory. Run a 'shotgun routes.rb' after navigating to directory 'build_api'
+in bash.
+-------------------------
+Via Postman app, able to CRUD sodas into an SQL database.
+
+Use ' get '/api/sodas/:name'' to query and sort by name of soda.
+
+Use 'post '/api/sodas'' to query and insert a soda by name and sugar_content int
+
+Use 'put '/api/sodas/:name'' to query and insert an updated sugar_content based
+on a soda's specific name.  This will be regulated and capitalized.  No error
+message as of now.
+
+Use 'delete '/api/sodas/:name'' to query and delete a row from the SQL db by
+soda name.
+-------------------------
+contact through GitHub for any troubleshooting or errors.
--- routes.rb
+++ routes.rb
@@ -5,24 +5,53 @@ require 'json'
 require 'active_record'
 
 get '/' do
-  'hi mom'
+  File.read(File.join('public', 'index.html'))
 end
 
+# playing with sorting.
+# get '/api/sodas' do
+#   name = params['name']
+#   sugar_content = params['sugar_content']
+#     if !name.nil? && !role.nil?
+#       sodas = Soda.where(name: name, sugar_content: sugar_content)
+#     elsif !name.nil?
+#       sodas = Soda.search(params[:name]).order("created_at DESC")
+#     elsif !role.nil?
+#       sodas = Soda.where(sugar_content: sugar_content)
+#     else
+#       sodas = Soda.all.order('created_at DESC')
+#     end
+#   sodas.to_json
+# end
+# @sodas = Soda.all
+# if params[:name]
+#   @sodas = Soda.search(params[:name]).order("created_at DESC")
+# else
+#   @sodas = Soda.all.order('created_at DESC')
+# end
+
 get '/api/sodas' do
+  # p File.read(File.join('public', '../public/api/sodas.html')) #playing with html
   Soda.all.to_json
 end
 
 get '/api/sodas/:name' do
-  Soda.find(params[:name])
+  Soda.find_by(name: params[:name]).to_json
 end
 
 post '/api/sodas' do
   Soda.create(name: params[:name].capitalize, sugar_content: params[:sugar_content]).to_json
 end
 
-# post '/api/sodas' do
-#   update_column(sugar_content, params[:sugar_content])
-# end
+put '/api/sodas/:name' do
+  soda = Soda.find_by(name: params[:name])
+  soda.update(sugar_content: params[:sugar_content]).to_json
+end
+
+delete '/api/sodas/:name' do
+  soda = Soda.find_by(name: params[:name])
+  soda.destroy
+end
 
 #refernce a task, input something
 
